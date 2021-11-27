6ae1baa28cc8419565a76873454975aae1073eea
Vgeorgee <cracken1111@gmail.com>
2021-11-27 20:38:34 +0100

    feat: add initial Country class

---
+++ covid_map/Country.java
@@ -0,0 +1,15 @@
+public class Country {
+    private Object shape;
+    private String name;
+    private boolean selected;
+
+    public Country(Object shape, String name) {
+        this.shape = shape;
+        this.name = name;
+    }
+
+    private void draw(){
+
+    }
+
+}
