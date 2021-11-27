54d8884f7630b09b5b29d6409f7389b915f774aa
Vgeorgee <cracken1111@gmail.com>
2021-11-27 21:42:06 +0100

    feat: add population loader

--- covid_map/DataLoader.java
+++ covid_map/DataLoader.java
@@ -1,14 +1,44 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class DataLoader {
-    static int[][] loadCases(String fileName){
+    static int[][] loadCases(String fileName) {
+        final Mapper mapper = Mapper.getInstance();
+        Scanner scanner;
+        try{
+            final File file = new File(fileName);
+            scanner = new Scanner(file);
+        } catch (Exception ex){
+            System.out.println(ex.toString());
+            return new int[1][1];
+        }
         return new int[1][1];
     }
     static int[][] loadDeaths(String fileName){
+        final Mapper mapper = Mapper.getInstance();
         return new int[1][1];
     }
     static int[][] loadVaccinations(String fileName){
+        final Mapper mapper = Mapper.getInstance();
         return new int[1][1];
     }
     static int[] loadPopulation(String fileName){
-        return new int[1];
+        final Mapper mapper = Mapper.getInstance();
+        Scanner scanner;
+        try{
+            final File file = new File(fileName);
+            scanner = new Scanner(file);
+        } catch (Exception ex){
+            System.out.println(ex.toString());
+            return new int[1];
+        }
+        final int NUMBER_OF_COUNTRIES = 39;
+        final int[] result = new int[NUMBER_OF_COUNTRIES];
+        scanner.nextLine();
+        while(scanner.hasNextLine()){
+            String[] tokens = scanner.nextLine().split(",");
+            result[mapper.countryIndex(tokens[0])] = Integer.parseInt(tokens[1]);
+        }
+        return result;
     }
 }
