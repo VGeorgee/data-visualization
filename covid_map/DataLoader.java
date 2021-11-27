17a170154e02307badeb86b84e44307032001b35
Vgeorgee <cracken1111@gmail.com>
2021-11-27 21:28:23 +0100

    feat: add base DataLoader class

---
+++ covid_map/DataLoader.java
@@ -0,0 +1,14 @@
+public class DataLoader {
+    static int[][] loadCases(String fileName){
+        return new int[1][1];
+    }
+    static int[][] loadDeaths(String fileName){
+        return new int[1][1];
+    }
+    static int[][] loadVaccinations(String fileName){
+        return new int[1][1];
+    }
+    static int[] loadPopulation(String fileName){
+        return new int[1];
+    }
+}
