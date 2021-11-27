dd13a7f41b9f337e1e58d019e4b09f0552ab32d0
Vgeorgee <cracken1111@gmail.com>
2021-11-27 23:03:34 +0100

    feat: add cases loader

--- covid_map/DataLoader.java
+++ covid_map/DataLoader.java
@@ -2,6 +2,10 @@ import java.io.File;
 import java.util.Scanner;
 
 public class DataLoader {
+
+    final static int NUMBER_OF_COUNTRIES = 39;
+    final static int MAX_NUMBER_OF_DATA = 700;
+
     static int[][] loadCases(String fileName) {
         final Mapper mapper = Mapper.getInstance();
         Scanner scanner;
@@ -12,7 +16,22 @@ public class DataLoader {
             System.out.println(ex.toString());
             return new int[1][1];
         }
-        return new int[1][1];
+
+        final int[][] result = new int[39][MAX_NUMBER_OF_DATA];
+
+        String[] header = scanner.nextLine().split(",");
+        while(scanner.hasNextLine()){
+            String[] tokens = parseLine(scanner.nextLine());
+            final int dateIndex = mapper.dateIndex(tokens[0]);
+
+            for(int i = 1; i < header.length; i++){
+                final int countryIndex = mapper.countryIndex(header[i]);
+                int value = tokens[i].equals("") ? 0 : Integer.parseInt(tokens[i]);
+                result[countryIndex][dateIndex] = result[countryIndex][dateIndex - 1] + value;
+            }
+        }
+
+        return result;
     }
     static int[][] loadDeaths(String fileName){
         final Mapper mapper = Mapper.getInstance();
@@ -32,13 +51,24 @@ public class DataLoader {
             System.out.println(ex.toString());
             return new int[1];
         }
-        final int NUMBER_OF_COUNTRIES = 39;
         final int[] result = new int[NUMBER_OF_COUNTRIES];
         scanner.nextLine();
         while(scanner.hasNextLine()){
             String[] tokens = scanner.nextLine().split(",");
             result[mapper.countryIndex(tokens[0])] = Integer.parseInt(tokens[1]);
         }
+        return result;
+    }
+
+    static String[] parseLine(String line){
+        Scanner sc = new Scanner(line);
+        sc.useDelimiter(",");
+        String[] result = new String[1000];
+        int resultSize = 0;
+        while (sc.hasNext()) {
+            result[resultSize++] = sc.next();
+        }
+        result[resultSize++] = "";
         return result;
     }
 }
