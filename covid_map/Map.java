d1519f5a43dcd3830c2e54094962b7d4f4557049
Vgeorgee <cracken1111@gmail.com>
2021-11-28 11:11:31 +0100

    feat: add basic api

--- covid_map/Map.java
+++ covid_map/Map.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Map {
-    private ArrayList<Country> shapeList;
-    private HashMap<String, Country> shapeMap;
+    private ArrayList<Country> countryList;
+    private HashMap<String, Country> countryMap;
     private Selector selector;
+    private DataHandler dataHandler;
 
     private int offsetX;
     private int offsetY;
@@ -14,35 +15,57 @@ public class Map {
     }
 
     public void select(String countryName) {
-        final Country country = shapeMap.get(countryName);
+        final Country country = countryMap.get(countryName);
         if(country == null){
             System.out.println("Country not found[" + countryName + "]");
         } else {
-            country.setSelected(true);
             selector.add(country);
         }
     }
 
     public void unselect(String countryName) {
-        final Country country = shapeMap.get(countryName);
+        final Country country = countryMap.get(countryName);
         if(country == null){
             System.out.println("Country not found[" + countryName + "]");
         } else {
-            country.setSelected(false);
             selector.remove(country);
         }
     }
 
 
     public void draw(){
+        // /*
+        for(Country shape: countryList){
+            shape.draw();
+        }
+        // */
+    }
 
+    public void loadMap(String fileName){
+        /*
+       PShape map = loadShape(fileName);
+       for(int i = 0; i < map.getChildCount(); i++) {
+            PShape child = map.getChild(i);
+           Country country = new Country(child, child.getName());
+           shapeList.add(country);
+           shapeMap.put(country.getName(), country);
+        }
+        //*/
+    }
+
+    public void setInterval(int start, int end){
+        dataHandler.setInterval(start, end);
+        for(Country country: this.countryList) {
+
+        }
     }
 
 
     private Map(){
-        shapeList = new ArrayList<>();
-        shapeMap = new HashMap<>();
+        countryList = new ArrayList<>();
+        countryMap = new HashMap<>();
         selector = Selector.getInstance();
+        dataHandler = DataHandler.getInstance();
     }
     private static Map INSTANCE;
     public static Map getInstance(){
