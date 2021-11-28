bb88d658c17cfff7318cb96c87b3f5f5d3ae561a
Vgeorgee <cracken1111@gmail.com>
2021-11-28 11:09:36 +0100

    feat: add colors and basic draw function

--- covid_map/Country.java
+++ covid_map/Country.java
@@ -1,21 +1,50 @@
-import java.util.Objects;
-
 public class Country {
     private Object shape;
     private String name;
-    private boolean selected;
+    private boolean isSelected;
+    private boolean isHover;
+    private int colour;
+
+
+    static final int HOVER_BACKGROUND = 0xCC80AAFF;
+    //static final int UNHOVER_BACKGROUND = 0xCC80AAFF;
+    static final int SELECTED_STROKE = 0xCC00CCFF;
+    static final int UNSELECTED_STROKE = 0xFFFFFFFF;
 
     public Country(Object shape, String name) {
         this.shape = shape;
         this.name = name;
     }
 
-    public void setSelected(boolean selected) {
-        this.selected = selected;
+    public String getName(){
+        return name;
     }
 
-    private void draw(){
+    public void setSelected(boolean selected) {
+        this.isSelected = selected;
+    }
 
+    public void setColour(int colour){
+        this.colour = colour;
+    }
+
+    public void draw(){
+         /*
+        if(isSelected){
+            shape.setStroke(SELECTED_STROKE);
+        } else {
+            shape.setStroke(UNSELECTED_STROKE);
+        }
+        if(isHover){
+            shape.setFill(HOVER_BACKGROUND);
+        } else {
+            shape.setFill(colour);
+        }
+         // */
+    }
+
+    public boolean isSelected(){
+        return isSelected;
     }
 
 
